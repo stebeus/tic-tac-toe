59af730eff2041980ea105a3908515c9b5f32887
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-07 16:42:12 -0300

    Create board matrix generator loop

--- app.js
+++ app.js
@@ -3,3 +3,10 @@
 const rows = 3;
 const columns = 3;
 const board = [];
+
+for (let row = 0; row < rows; row++) {
+  board[row] = [];
+  for (let column = 0; column < columns; column++) {
+    board[row].push(0);
+  }
+}
