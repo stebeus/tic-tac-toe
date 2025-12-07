1baf33e16eba074a2ef31bc86bd9358b79934e01
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-07 16:49:58 -0300

    Create mark function

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ const createPlayer = (integer, score) => ({ integer, score });
 
 const playerOne = createPlayer(1, 0);
 const playerTwo = createPlayer(2, 0);
+
+function mark(row, column) {
+  if (row >= board.length || column >= board.length) {
+    console.error("The indicated row/column is bigger than the board");
+    return;
+  }
+
+  if (board[row][column] !== 0) {
+    console.error(`The row ${row} in column ${column} is already marked`);
+    return;
+  }
+
+  board[row][column] = playerOne.integer;
+  console.table(board);
+}
