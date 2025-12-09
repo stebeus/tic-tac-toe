e60e71634ebac8f5bdd6fb5003faca6056e2a488
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-09 17:40:25 -0300

    Include return in mark function restart to fix pre-marking bug

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const GameController = (function () {
       throw Error(`The row ${row} in column ${col} is already marked`);
     }
 
-    if (checkWin() || checkTie()) restart();
+    if (checkWin() || checkTie()) return restart();
 
     board[row][col] = currPlayer;
     switchTurn();
