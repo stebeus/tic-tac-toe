4bb6cc16e7325e721df4d59fda7e57841e8dfc46
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-09 13:01:37 -0300

    Wrap the game components in GameController IIFE module

--- app.js
+++ app.js
@@ -25,73 +25,77 @@ function createPlayer(integer) {
   return { integer, getScore, increaseScore };
 }
 
-const board = GameBoard.board;
-const players = [createPlayer(1), createPlayer(2)];
+const GameController = (function () {
+  const board = GameBoard.board;
+  const players = [createPlayer(1), createPlayer(2)];
 
-let currentPlayer = players[0];
+  let currentPlayer = players[0];
 
-function switchPlayerTurn() {
-  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  function switchPlayerTurn() {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    console.log(`PLAYER ${currentPlayer.integer} TURN`);
+  }
+
+  function playRound() {
+    console.clear();
+
+    switch (true) {
+      case checkWin(currentPlayer.integer):
+        currentPlayer.increaseScore();
+        console.log(`PLAYER ${currentPlayer.integer} WON!`);
+        break;
+
+      case checkTie():
+        console.log(`TIE!`);
+        break;
+
+      default:
+        switchPlayerTurn();
+        break;
+    }
+
+    GameBoard.print();
+  }
+
+  function mark(row, column) {
+    if (row >= board.length || column >= board.length) {
+      throw Error("The selected row or column does not exist");
+    }
+
+    if (board[row][column] !== 0) {
+      throw Error(`The row ${row} in column ${column} is already marked`);
+    }
+
+    board[row][column] = currentPlayer.integer;
+    playRound();
+  }
+
+  function checkWin(integer) {
+    if (board.some(row => row.every(cell => cell === integer))) {
+      return true;
+    }
+
+    if (board[0].some(column => board.every(row => row[column] === integer))) {
+      return true;
+    }
+
+    if (
+      board.every((row, column) => row[column] === integer) ||
+      board.every((row, column) => row[board.length - 1 - column] === integer)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function checkTie() {
+    return board.every(row => row.every(cell => cell !== 0));
+  }
+
+  console.log("Use mark(row,column) to select a space in the board");
   console.log(`PLAYER ${currentPlayer.integer} TURN`);
-}
-
-function playRound() {
-  console.clear();
-
-  switch (true) {
-    case checkWin(currentPlayer.integer):
-      currentPlayer.increaseScore();
-      console.log(`PLAYER ${currentPlayer.integer} WON!`);
-      break;
-
-    case checkTie():
-      console.log(`TIE!`);
-      break;
-
-    default:
-      switchPlayerTurn();
-      break;
-  }
-
   GameBoard.print();
-}
 
-function mark(row, column) {
-  if (row >= board.length || column >= board.length) {
-    throw Error("The selected row or column does not exist");
-  }
-
-  if (board[row][column] !== 0) {
-    throw Error(`The row ${row} in column ${column} is already marked`);
-  }
-
-  board[row][column] = currentPlayer.integer;
-  playRound();
-}
-
-function checkWin(integer) {
-  if (board.some(row => row.every(cell => cell === integer))) {
-    return true;
-  }
-
-  if (board[0].some(column => board.every(row => row[column] === integer))) {
-    return true;
-  }
-
-  if (
-    board.every((row, column) => row[column] === integer) ||
-    board.every((row, column) => row[board.length - 1 - column] === integer)
-  ) {
-    return true;
-  }
-
-  return false;
-}
-
-function checkTie() {
-  return board.every(row => row.every(cell => cell !== 0));
-}
-
-console.log("Use mark(row,column) to select a space in the board");
-console.log(`PLAYER ${currentPlayer.integer} TURN`);
-GameBoard.print();
+  return { mark };
+})();
